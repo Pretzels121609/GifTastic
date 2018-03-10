d8549802a2b5c8586a320f82a1cb59f67a1f115e
Kathy <Kathy@Kathy-Giebelhausens-iMac.local>
2018-03-09 21:44:50 -0700

    html and jQuery, no css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,106 @@
 //set up variables
 //need the api key
+console.log("hello")
 
-var authKey = "SMAj066wT0ihGKG0qdObtj1dx2Gpb9cb";
 
 //array of items for buttons
-var SeaCreatures = ["dolphin", "crab", "shark", "turtle", "whale", "jellyfish", "octopus", "sea " + "lion", "swordfish", "tuna", "shrimp", "seahorse"];
-console.log(SeaCreatures)
+var seaCreatures = ["dolphin", "crab", "shark", "turtle", "sea lion"];
+console.log(seaCreatures)
+
+//1
+// create a function that loops though the seaCreatures array
+// for each item in the array create a button 
+// add the item text to the button
+// add classes to the button
+// before appending empty current content in the buttonwrap
+// append each button the the buttonwrap
+function createButton(){
+    $("#buttonwrap").empty()
+    for (var i = 0; i < seaCreatures.length; i++){
+        var button = $("<button>") 
+        button.text(seaCreatures[i])
+        button.addClass("btn btn-primary option")
+        $("#buttonwrap").append(button)
+    }
+}
+createButton();
+
+
+//2 
+// on click of #submit 
+//  grab the input #search value
+// store value in a variable ie newSeaCreature
+
+// conditional
+    // check if newSeaCreature is already in the seaCreature array
+    // if its not there add it
+    // call funtion in step 1
+    // clear the input 
+    // if it is there tell the user to try something else; use #alert
+    // clear the input 
+$("#submit").on("click", function(event){
+    event.preventDefault()
+    var userInput = $("#search").val().trim()
+    userInput = userInput.toLowerCase()
+    if (seaCreatures.indexOf(userInput) === -1 && userInput.length > 0)  // 0- n // -1 this will check if the user input is in the array
+    {
+        seaCreatures.push(userInput)
+        createButton()
+        $("#search").val("")
+    }
+    else {
+        $("#alert").text("try something else")
+        $("#search").val("")
+    }
+})
+
+
+//3 
+// on click on #search remove any text from #alert
+$("#search").on("click", function(){
+    $("#alert").text(" ")
+})
+
+
+//4 on click of .option button 
+// get button text store to a variable
+// create api query
+// make ajax call
+// console log result
+$(document).on("click", ".option", function(){ //only use when any element you click on is added dynamically
+    var selectButton = $(this).text()
+    console.log(selectButton)
+    var authKey = "&apikey=SMAj066wT0ihGKG0qdObtj1dx2Gpb9cb";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selectButton + "&limit=10" + authKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(results){
+        console.log(results.data)
+        var gifsArry = results.data
+        // call the funvtion in step 5, pass in gifsArry
+    })
+})
+
+//5
+// create a function that take gifsArry as a argument 
+// loop through the results.data
+// create a div
+// create a p tag
+// get the rating
+// add the rating to the p tag
+// create an image tag
+// add an attr call src
+// get the still link
+// add the still link  to the src attr
+// add attr call data-still and also add the still link to it
+// add an attr call data-animate
+// get the animated link
+// add the animated link to it
+// add an attr call data-state its value is "still"
+// append each img and p tag to the div created l:86
+// append the div to #gifwrap
+
+
+// 6
+// animate and make is still
