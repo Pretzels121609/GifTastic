3aed03c4cc15ce9eaf21cef74b1406ddf8b88187
Kathy <Kathy@Kathy-Giebelhausens-iMac.local>
2018-03-09 17:24:16 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//set up variables
+//need the api key
+
+var authKey = "SMAj066wT0ihGKG0qdObtj1dx2Gpb9cb";
+
+//array of items for buttons
+var SeaCreatures = ["dolphin", "crab", "shark", "turtle", "whale", "jellyfish", "octopus", "sea " + "lion", "swordfish", "tuna", "shrimp", "seahorse"];
+console.log(SeaCreatures)
